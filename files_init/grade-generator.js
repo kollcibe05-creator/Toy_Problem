64a9c02d64151d1f94c339ae693e0d71a6e326d0
Collins Kibet <kollcibe05@gmail.com>
2025-09-09 11:13:29 +0300

    ADDSmyfiles

---
+++ files_init/grade-generator.js
@@ -0,0 +1,19 @@
+   function greet(marks){ 
+    if (marks>79 ){
+        return console.log("Grade:A" )     
+    }
+    else if (marks>=60) {
+        return console.log("Grade:B")
+    }
+    else if (marks>=49) {
+        return console.log("Grade:C")
+    }
+    else if (marks>=40) {
+        return console.log( "Grade:D")
+    }
+    else if (marks<40) {
+        return console.log( "Grade:E")
+    }
+}
+
+    (grade(60))
